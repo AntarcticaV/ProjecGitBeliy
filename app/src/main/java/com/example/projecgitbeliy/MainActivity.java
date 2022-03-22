44586268ccce67661c340adeffdfc4aae439076c
Roman <beliyroman@outlook.com>
2022-03-22 19:29:58 +0700

    Comment line 11 "Fourth comment"

--- app/src/main/java/com/example/projecgitbeliy/MainActivity.java
+++ app/src/main/java/com/example/projecgitbeliy/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override//Third comment
     protected void onCreate(Bundle savedInstanceState) {//Second comment
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);//Fourth comment
         setContentView(R.layout.activity_main);//First comment
     }
 }
